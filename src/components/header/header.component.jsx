87f7aa80f3b87b2e5868f7a8f3c21c257793b791
Vicente Pirozzi <vapirozzi@uc.cl>
2020-03-26 00:22:45 -0300

    Added UserActionTypes and Redirect if SignIn already

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -35,7 +35,7 @@ const Header = ({ currentUser }) => {
           </Link>
         ) : (
           <Link className="option" to="/signIn">
-            SIGN IN{" "}
+            SIGN IN
           </Link>
         )}
       </div>
