83fc2b09420847fda3b2f77ce1c2edfc9de13fd4
Vicente Pirozzi <vapirozzi@uc.cl>
2020-03-29 17:33:31 -0300

    added more styled components and withSpinner HOC to some components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import "./header.styles.scss";
-
 import { connect } from "react-redux";
-
-import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/icon.svg";
 import { auth } from "../../firebase/firebase.utils";
@@ -15,38 +11,34 @@ import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => {
   let location = useLocation();
   return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <Logo className="logo" />
-      </Link>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
-        </Link>
-        <Link className="option" to="/contact">
-          CONTACT
-        </Link>
+    <HeaderContainer>
+      <LogoContainer to="/">
+        <Logo />
+      </LogoContainer>
+      <OptionsContainer>
+        <OptionLink to="/shop">SHOP</OptionLink>
+        <OptionLink to="/contact">CONTACT</OptionLink>
         {currentUser ? (
-          <Link
-            className="option"
-            to={location.pathname}
-            onClick={() => auth.signOut()}
-          >
+          <OptionLink to={location.pathname} onClick={() => auth.signOut()}>
             SIGN OUT{" "}
-          </Link>
+          </OptionLink>
         ) : (
-          <Link className="option" to="/signIn">
-            SIGN IN
-          </Link>
+          <OptionLink to="/signIn">SIGN IN</OptionLink>
         )}
         <CartIcon />
-      </div>
+      </OptionsContainer>
       {hidden ? null : <CartDropdown />}
-    </div>
+    </HeaderContainer>
   );
 };
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => <Directory />;
+import { HomePageContainer } from "./homepage.styles";
+
+const HomePage = () => (
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
+);
 
 export default HomePage;
